76b8945e3ecb463423e10a713917302bdb2a96b6
abdullah05-afk <abdullahmunir05@gmail.com>
2024-11-19 12:31:45 +0500

    container config done

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <h1>Hello Chai_aur_React | Hafiz Abdullah I install the react with npx create react app </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  
+
+  return (
+    <h1>Chai_aur_React with vite | Abdullah</h1>
+  )
+}
+
+export default App
