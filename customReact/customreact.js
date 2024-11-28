269acbed64742c239fadbcde8e147544c7db5494
abdullah05-afk <abdullahmunir05@gmail.com>
2024-11-28 15:18:33 +0500

    container config done

---
+++ customReact/customreact.js
@@ -0,0 +1,21 @@
+function customRender(reactElement,container){
+    const domElement = document.create
+}
+
+
+const reactElement = {
+    type: 'a',
+    props:{
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+
+
+const mainContainer  = document.querySelector('#root');
+
+
+customRender(reactElement,mainContainer);
+
